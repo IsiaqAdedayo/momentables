491108332ef1837506a35ee53c0d5e652fd83cd5
Showande_Adedayo <hadedaryor@gmail.com>
2021-09-15 13:34:05 +0100

    updated commit

--- components/marketPlace.jsx
+++ components/marketPlace.jsx
@@ -2,7 +2,7 @@
 
 const MarketPlace = () => {
     return (
-        <div className="px-6 py-3">
+        <div className="px-6 py-3 md:px-28">
             <div className="flex flex-col justify-center items-center py-14 font-bold text-center space-y-4">
                 <h1 className="text-xs uppercase leading-loose tracking-widest md:text-xl"><span className="text-2xl">M</span>arketplace</h1>
                 <h1 className="text-2xl md:text-6xl md:pb-20">Lorem ipsum este dolor</h1>
